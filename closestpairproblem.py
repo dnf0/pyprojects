ceca41b26165d897ec19ca76a3bf3f8c3a46cd28
dabillox <dnf@mssl.ucl.ac.uk>
2013-11-01 09:29:12 -0500

    closest pair problem updated to include >2 dimensions.

--- closestpairproblem.py
+++ closestpairproblem.py
@@ -2,6 +2,10 @@
 Given an input array finds the closest pair.  Achieves matching using scipy's cKDTree, very quick even for 
 millions of points.  Speed ups may be achieved using pyflann: 
 https://github.com/mariusmuja/flann/tree/master/src/python/pyflann  
+
+Here only 2-dimensional for plotting purposes. Extensible to more by changing the dims
+parameter.  Large numbers of dimensions will experience a decline in performance, see
+http://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.cKDTree.html 
 '''
 import numpy as np
 import matplotlib.pyplot as plt
@@ -38,19 +42,22 @@ def main():
     #get grid size
     print 'please enter number of random points: '
     n_points = int(raw_input())
+    dims = 2
     
     #make random x and y values
-    random_points = np.random.rand(n_points,2) 
+    random_points = np.random.rand(n_points,dims) 
     
     #show the points
-    print 'plotting figure... please close to proceed'
-    show_it(random_points, None)
+    if dims == 2:
+        print 'plotting figure... please close to proceed'
+        show_it(random_points, None)
     
     #find the closest point pair
     closest_pair_index = get_closest_pair(random_points)
     
     #replot
-    show_it(random_points, closest_pair_index)
+    if dims ==2:
+        show_it(random_points, closest_pair_index)
     
 if __name__ == '__main__':
     sys.exit(main())
